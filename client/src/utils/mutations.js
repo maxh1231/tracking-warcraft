12f529588bf04e7183fa542381bd38477b562c29
maxh1231 <maxhumpherys@gmail.com>
2022-03-08 20:17:33 -0700

    client side signup, login, addtoken mutations

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,38 @@
+import { gql } from '@apollo/client';
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_BLIZZTOKEN = gql`
+  mutation addToken($token: String!) {
+    addToken(token: $token) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
