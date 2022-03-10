85e6a7f53cdd6882e3a3f388c7733b8da3e6fd8e
maxh1231 <maxhumpherys@gmail.com>
2022-03-10 00:31:14 -0700

    in app access_token variable conditionally set to DB access_token or fetch access_token

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useQuery, useMutation } from '@apollo/client';
 import { QUERY_TOKEN } from '../utils/queries';
 
@@ -6,18 +7,30 @@ import { QUERY_TOKEN } from '../utils/queries';
 const Home = () => {
     const { loading, data } = useQuery(QUERY_TOKEN);
 
-    if (!loading) {
-        if (data.getToken.length === 0) {
-            fetch("https://us.battle.net/oauth/token", {
-                body: "grant_type=client_credentials",
-                headers: {
-                    Authorization: `Basic ${process.env.REACT_APP_client_id_secret}=`,
-                    "Content-Type": "application/x-www-form-urlencoded"
-                },
-                method: "POST"
-            }).then(response => response.json()).then(fetchData => console.log(fetchData.access_token))
+    let access_token;
+    useEffect(() => {
+        if (!loading) {
+            if (data.getToken.length === 0) {
+                fetch("https://us.battle.net/oauth/token", {
+                    body: "grant_type=client_credentials",
+                    headers: {
+                        Authorization: `Basic ${process.env.REACT_APP_client_id_secret}=`,
+                        "Content-Type": "application/x-www-form-urlencoded"
+                    },
+                    method: "POST"
+                }).then(response => response.json())
+                    .then((fetchData) => {
+                        console.log(fetchData.access_token)
+                        access_token = fetchData.access_token
+                    })
+            } else {
+                access_token = data.getToken[0].access_token;
+            }
         }
-        console.log(data);
+    }, [])
+
+    if (!loading) {
+        console.log(access_token);
     }
 
     return (
