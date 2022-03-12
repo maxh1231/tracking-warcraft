066110bf3ec029c6a2b57319036a13088f242011
maxh1231 <maxhumpherys@gmail.com>
2022-03-12 00:22:51 -0700

    arr.map properly without infinite loop or not displaying retrieved data

--- client/src/pages/SearchResults.js
+++ client/src/pages/SearchResults.js
@@ -1,23 +1,52 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const SearchResults = () => {
+    const [characters, setCharacters] = useState(null)
+
+    useEffect(() => {
+        getData();
+
+        async function getData() {
+            const response = await fetch(`https://hidden-retreat-58836.herokuapp.com/https://raider.io/api/search?term=${field.name}`)
+            const data = await response.json();
+
+            setCharacters(data)
+        }
+    }, [setCharacters])
 
     let field = useParams();
     console.log(field);
 
-    let charArr = [];
-    fetch(`https://hidden-retreat-58836.herokuapp.com/https://raider.io/api/search?term=${field.name}`).then(response => response.json()).then(charData => {
-        console.log(charData)
-        for (let i = 0; i < charData.matches.length; i++) {
-            charArr.push(charData.matches[i])
+    // let charArr = [];
+    // fetch(`https://hidden-retreat-58836.herokuapp.com/https://raider.io/api/search?term=${field.name}`).then(response => response.json()).then(charData => {
+    //     console.log(charData)
 
-        }
-    })
+    // })
+    //     console.log(setCharacters)
+    //     for (let i = 0; i < charData.matches.length; i++) {
+    //         charArr.push(charData.matches[i])
 
-    console.log(charArr);
+    //     }
+    // })
+
+
+
+    console.log(characters);
     return (
 
         <section>
+
+            {characters && (
+                <div>
+                    {characters.matches.map((character, index) => (
+                        <div key={index}>
+                            <h2>{character.name}</h2>
+                        </div>
+                    ))}
+                </div>
+            )}
+
             {/* {(charArr.length > 0) ? (
                 {
                     charArr.map((character) => (
@@ -28,12 +57,12 @@ const SearchResults = () => {
                     ))
                 )} : <span>no results</span>} */}
 
-            <div>
+            {/* <div>
                 {(charArr.length > 0) ? (charArr.map((character) => (
                     <span key={character.data.id}></span>
                 ))) : (<span>No results</span>)}
 
-            </div>
+            </div> */}
         </section>
     )
 }
