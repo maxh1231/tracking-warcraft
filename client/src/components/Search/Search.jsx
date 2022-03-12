14e93a4cb8e3d1679e20d83777b3990754dc623f
maxh1231 <maxhumpherys@gmail.com>
2022-03-11 21:07:13 -0700

    raiderio query for multiple character results

--- client/src/components/Search/Search.jsx
+++ client/src/components/Search/Search.jsx
@@ -7,14 +7,24 @@ const Search = ({ access_token }) => {
         setField(event.target.value);
     }
 
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        fetch(`https://hidden-retreat-58836.herokuapp.com/https://raider.io/api/search?term=${field}`)
+    }
+
+    // fetch(`https://us.api.blizzard.com/data/wow/search/character/max/realm/?namespace=profile-us&access_token=${access_token}`)
+
+
+
     console.log(access_token)
     return (
         <section>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input
                     type="text"
                     onChange={handleChange}
                 />
+                <button type='submit'>Submit</button>
             </form>
         </section>
     )
