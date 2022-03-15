55412fe331e93d8a664a635898ec807d2e60a021
maxh1231 <maxhumpherys@gmail.com>
2022-03-14 19:18:36 -0600

    IO fetch of character

--- client/src/pages/CharacterPage.js
+++ client/src/pages/CharacterPage.js
@@ -1,7 +1,11 @@
 import { useParams } from "react-router-dom"
 import { BlizzAPI } from 'blizzapi'
+import { useEffect, useState } from "react";
 
 const CharacterPage = () => {
+    const [character, setCharacter] = useState(null)
+
+
     const params = useParams();
     console.log(params)
 
@@ -11,13 +15,26 @@ const CharacterPage = () => {
         clientSecret: process.env.REACT_APP_client_secret,
     });
 
-    console.log(process.env.REACT_APP_client_id)
-    console.log(process.env.REACT_APP_client_secret)
+    // console.log(process.env.REACT_APP_client_id)
+    // console.log(process.env.REACT_APP_client_secret)
 
-    const data = api.query(`/profile/wow/character/tichondrius/maxh/equipment?namespace=profile-us&locale=en_US`)
-    console.log(data);
+    // const equipData = api.query(`/profile/wow/character/tichondrius/maxh/equipment?namespace=profile-us&locale=en_US`)
+    // console.log(equipData);
 
 
+    useEffect(() => {
+        ioFetch();
+
+        async function ioFetch() {
+            const response = await fetch('https://hidden-retreat-58836.herokuapp.com/https://raider.io/api/v1/characters/profile?region=us&realm=tichondrius&name=maxh&fields=gear')
+            const data = await response.json()
+
+            setCharacter(data)
+
+        }
+    }, [setCharacter])
+
+    console.log(character)
 
     return (
         <section>hi character page</section>
