30d4ac4a672a84a055e3d8c35893d87af074556a
maxh1231 <maxhumpherys@gmail.com>
2022-03-14 20:18:19 -0600

    renders character data

--- client/src/pages/CharacterPage.js
+++ client/src/pages/CharacterPage.js
@@ -8,6 +8,7 @@ const CharacterPage = () => {
 
     const params = useParams();
     const charName = params.name.toLowerCase()
+    const upperCaseRegion = params.region.toUpperCase();
     console.log(params)
 
     const api = new BlizzAPI({
@@ -27,7 +28,7 @@ const CharacterPage = () => {
         ioFetch();
 
         async function ioFetch() {
-            const response = await fetch(`https://hidden-retreat-58836.herokuapp.com/https://raider.io/api/v1/characters/profile?region=${params.region}&realm=${params.realm}&name=${charName}&fields=gear`)
+            const response = await fetch(`https://hidden-retreat-58836.herokuapp.com/https://raider.io/api/v1/characters/profile?region=${params.region}&realm=${params.realm}&name=${charName}&fields=gear%2Ccovenant%2Craid%2Cguild`)
             const data = await response.json()
 
             setCharacter(data)
@@ -38,7 +39,31 @@ const CharacterPage = () => {
     console.log(character)
 
     return (
-        <section>hi character page</section>
+        <section>
+
+            {character !== null &&
+                <div>
+                    <div>
+                        <img src={character.thumbnail_url} ></img>
+                    </div>
+                    <div>
+                        <div>
+                            <span>{character.name}</span>
+                        </div>
+                        {character.guild !== null && (
+                            <div>
+                                <span>{"<"}{character.guild.name}{">"}</span>
+                            </div>
+                        )}
+                        <div>
+                            <span>({upperCaseRegion}) {character.realm}</span>
+                        </div>
+                        <div>
+                            <span>{character.race}<span> {character.active_spec_name} {character.class}</span></span>
+                        </div>
+                    </div>
+                </div>}
+        </section>
     )
 }
 
