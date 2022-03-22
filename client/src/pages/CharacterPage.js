7160753c2434348e5e0d9a0c296ddbbd5acdd022
maxh1231 <maxhumpherys@gmail.com>
2022-03-21 22:55:40 -0600

    spires of ascension

--- client/src/pages/CharacterPage.js
+++ client/src/pages/CharacterPage.js
@@ -594,13 +594,17 @@ const CharacterPage = () => {
                             <td>Spires of Ascension</td>
                             {dungeons !== null && (
                                 dungeons.filter(dungeon => dungeon.dungeon.includes('Spires of Ascension') && dungeon.affixes[0].name.includes('Tyrannical')).map(item => (
-                                    <td>is tyrannical</td>
+                                    item.affixes.map((affix) => (
+                                        <td><a href="#" data-wowhead={affix.wowhead_url}><img src={`https://wow.zamimg.com/images/wow/icons/medium/${affix.icon}.jpg`}></img></a></td>
+                                    ))
                                 ))
                             )}
 
                             {dungeons !== null && (
                                 dungeons.filter(dungeon => dungeon.dungeon.includes('Spires of Ascension') && dungeon.affixes[0].name.includes('Fortified')).map(item => (
-                                    <td>is fortified</td>
+                                    item.affixes.map((affix) => (
+                                        <td><a href="#" data-wowhead={affix.wowhead_url}><img src={`https://wow.zamimg.com/images/wow/icons/medium/${affix.icon}.jpg`}></img></a></td>
+                                    ))
                                 ))
                             )}
                         </tr>
