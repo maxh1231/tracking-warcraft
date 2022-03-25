b497c3f8cf9c7caa97dd47632c4383120de831a3
maxh1231 <maxhumpherys@gmail.com>
2022-03-24 19:18:39 -0600

    commit

--- client/src/components/HybridClass/HybridClass.jsx
+++ client/src/components/HybridClass/HybridClass.jsx
@@ -21,7 +21,33 @@ const HybridClass = ({ equipment, currentSeason, setCurrentSeason }) => {
 
     return (
         <div>
+            <div>
+                <p>{equipment.mythic_plus_scores_by_season[currentSeason].scores.all}</p>
+                <p>Overall</p>
+                <p>Mythic+ Score</p>
+            </div>
 
+            <div>
+                <p>{scores[0].score}</p>
+                <p>{scores[0].name}</p>
+                <p>Mythic+ Score</p>
+            </div>
+
+            {scores[1].score > 0 && (
+                <div>
+                    <p>{scores[1].score}</p>
+                    <p>{scores[1].name}</p>
+                    <p>Mythic+ Score</p>
+                </div>
+            )}
+
+            {scores[2].score > 0 && (
+                <div>
+                    <p>{scores[2].score}</p>
+                    <p>{scores[2].name}</p>
+                    <p>Mythic+ Score</p>
+                </div>
+            )}
         </div>
     )
 }
