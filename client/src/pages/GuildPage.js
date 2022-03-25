f45bab4260b3707b9f995cdd62af10d688323446
maxh1231 <maxhumpherys@gmail.com>
2022-03-25 14:30:04 -0600

    guild data request setup

--- client/src/pages/GuildPage.js
+++ client/src/pages/GuildPage.js
@@ -1,4 +1,22 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom"
+
 const GuildPage = () => {
+    const params = useParams();
+    console.log(params);
+    const guildName = params.name.toLowerCase()
+
+    useEffect(() => {
+        guildData()
+
+        async function guildData() {
+            const response = await fetch(`https://raider.io/api/v1/guilds/profile?region=${params.region}&realm=${params.realm}&name=${guildName}&fields=raid_progression%2C%20raid_rankings`)
+
+            const data = await response.json()
+            console.log(data);
+        }
+    }, [])
+
     return (
         <section>hi guild page</section>
     )
