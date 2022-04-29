ad6c11a8bcae9006259cc9077a8f3fc7efb7b035
maxh1231 <maxhumpherys@gmail.com>
2022-04-28 18:53:55 -0600

    Runs leaderboard data fetch setup

--- client/src/components/RunLeaderboard/RunLeaderboard.jsx
+++ client/src/components/RunLeaderboard/RunLeaderboard.jsx
@@ -1,4 +1,23 @@
+import { useState, useEffect } from 'react'
+
 const RunLeaderboard = () => {
+    const [board, setBoard] = useState(null)
+
+    useEffect(() => {
+        runFetch()
+
+        async function runFetch() {
+            const response = await fetch(`https://raider.io/api/v1/mythic-plus/runs?season=season-sl-3&region=world&dungeon=all&page=0`);
+            const data = await response.json()
+
+            setBoard(data)
+
+        }
+
+    }, [setBoard])
+
+    console.log(board);
+
     return (
         <section></section>
     )
