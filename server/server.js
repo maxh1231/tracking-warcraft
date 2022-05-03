d8afa5990dc88322840fe61e9c0bea3bf9a468d5
Leithen <leithen113@gmail.com>
2022-05-03 00:08:34 -0600

    Added express session logic from MERN boiler

--- server/server.js
+++ server/server.js
@@ -1,44 +1,59 @@
 require('dotenv').config();
 const express = require('express');
-const app = express();
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 const { ApolloServer } = require('apollo-server-express');
-const { authMiddleware } = require('./utils/auth');
-const { GraphQLUpload, graphqlUploadExpress } = require('graphql-upload');
-const path = require('path');
-
 const { typeDefs, resolvers } = require('./schemas');
+const path = require('path');
 const db = require('./config/connection');
-const PORT = process.env.PORT || 3001;
+const authRouter = require('./controllers')
+const passport = require('./passport/setup');
+const { authMiddleware } = require('./utils/auth')
 
-const server = new ApolloServer({
-    uploads: false,
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+const startServer = async () => {
+  const server = new ApolloServer({
     typeDefs,
     resolvers,
     context: authMiddleware,
-});
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
+};
 
-app.use(graphqlUploadExpress());
-server.applyMiddleware({ app });
+startServer();
 
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(session({
+  secret: process.env.SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: MongoStore.create({
+    mongoUrl: 'mongodb://localhost/turtle',
+  })
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/', authRouter)
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
-}
+  app.use(express.static(path.join(__dirname, '../client/build')));
+};
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
-
-const { createServer } = require('http');
-const httpServer = createServer(app);
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
 
 db.once('open', () => {
-    console.log(
-        `GraphQL server ready at http://localhost:${PORT}${server.graphqlPath}`
-    );
-    httpServer.listen(PORT, () =>
-        console.log(`Listening on localhost:${PORT}`)
-    );
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
 });
