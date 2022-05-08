df1c7a646dc5acec85f9d97a7acd1d2fb67378b5
maxh1231 <maxhumpherys@gmail.com>
2022-05-08 13:04:33 -0600

    added filter options with state

--- client/src/pages/Leaderboard.js
+++ client/src/pages/Leaderboard.js
@@ -1,9 +1,18 @@
+import { useState } from 'react'
+
 import RunLeaderboard from "../components/RunLeaderboard";
 import RoleLeaderboard from "../components/RoleLeaderboard";
 
 const Leaderboard = () => {
+    const [filter, setFilter] = useState('run');
+
     return (
         <section>
+            <div>
+                <button>Runs</button>
+                <button>Role</button>
+                <button>Class</button>
+            </div>
             <div>
                 <RunLeaderboard />
             </div>
