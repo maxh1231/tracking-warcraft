424ca91f6ae6c254a5e062ce7ce322d538a71e56
maxh1231 <maxhumpherys@gmail.com>
2022-05-13 13:25:21 -0600

    moved Talents logic over to Talents component

--- client/src/components/CharacterPageComponents/Talents/Talents.jsx
+++ client/src/components/CharacterPageComponents/Talents/Talents.jsx
@@ -1,7 +1,22 @@
-const Talents = () => {
+import { v4 as uuid } from 'uuid';
+
+const Talents = ({ talents, equipment }) => {
     return (
         <section>
-
+            {(talents && equipment) !== null && (
+                <div>
+                    <div>
+                        <h3>Talents</h3>
+                    </div>
+                    <div style={{ display: 'flex' }}>
+                        {talents.map((item, index) => (
+                            <div key={uuid()}>
+                                <a href="#" data-wowhead={`spell=${item.spell_tooltip.spell.id}`}><img src={`https://wow.zamimg.com/images/wow/icons/medium/${equipment.talents[index].icon}.jpg`}></img></a>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            )}
         </section>
     )
 }
--- client/src/pages/CharacterPage.js
+++ client/src/pages/CharacterPage.js
@@ -135,21 +135,9 @@ const CharacterPage = () => {
 
             <CharacterInfo equipment={equipment} />
             <Gear equipment={equipment} />
+            <Talents equipment={equipment} talents={talents} />
 
-            {(talents && equipment) !== null && (
-                <div>
-                    <div>
-                        <h3>Talents</h3>
-                    </div>
-                    <div style={{ display: 'flex' }}>
-                        {talents.map((item, index) => (
-                            <div key={uuid()}>
-                                <a href="#" data-wowhead={`spell=${item.spell_tooltip.spell.id}`}><img src={`https://wow.zamimg.com/images/wow/icons/medium/${equipment.talents[index].icon}.jpg`}></img></a>
-                            </div>
-                        ))}
-                    </div>
-                </div>
-            )}
+
 
             {equipment !== null && (
                 <div>
